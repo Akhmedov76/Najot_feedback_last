67367699294d2ff702e8b6ba8feb914f137655f5
Akhmedov76 <ahmadovj5016@gmail.com>
2024-10-14 15:49:17 +0500

    changed

--- conf/settings.py
+++ conf/settings.py
@@ -123,13 +123,13 @@ CKEDITOR_CONFIGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Media settings
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Email settings
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
